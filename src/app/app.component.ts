f1c0a8e28ec6999975566ea5a69243572910a564
MdMuzaffar <54287360+MdMuzaffar@users.noreply.github.com>
2025-05-23 22:49:40 +0530

    chore: Validating form and importing values in UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,23 +6,40 @@ import {
   FormBuilder,
   FormGroup,
 } from '@angular/forms';
-
+import { CommonModule } from '@angular/common';
+interface User {
+  name: string;
+  email: string;
+}
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   userForm: FormGroup;
 
+  items: User[] = [];
+
   fb = inject(FormBuilder);
 
   constructor() {
     this.userForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
     });
+  }
+
+  submitForm() {
+    if (this.userForm.valid) {
+      const newUser: User = this.userForm.value;
+      this.items.push(newUser);
+      console.log(this.userForm.value);
+      this.items = this.userForm.value;
+      localStorage.setItem('user', this.userForm.value);
+      this.userForm.reset();
+    }
   }
 }
